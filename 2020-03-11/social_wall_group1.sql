8e08b268e65a9548d32802a173ee9ed9f8a3b105
Jesse Farmer <jesse@20bits.com>
2020-03-11 13:06:59 -0700

    Update social_wall_group1.sql

--- 2020-03-11/social_wall_group1.sql
+++ 2020-03-11/social_wall_group1.sql
@@ -1,14 +1,18 @@
+-- Add "likes" column to "messages" table
 ALTER TABLE messages
 ADD COLUMN likes INTEGER DEFAULT 0;
 
+-- Increment the "likes" column by 1 for the message with id = 1
 UPDATE messages
 SET likes = likes + 1
 WHERE id = 1;
 
+-- Create a new table called "likes"
 CREATE TABLE likes (
   id SERIAL PRIMARY KEY,
   message_id INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 );
 
+-- Insert a row into the "likes" table, corresponding to liking the message with id = 1
 INSERT INTO likes (message_id) VALUES (1);
